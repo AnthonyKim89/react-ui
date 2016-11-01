7d17431c9752b62afafe3052b1b4a946923af151
Anthony89 <anthony.kim.89@hotmail.com>
2016-11-01 14:30:51 +0200

    preliminary maximize/minimize widget functionality

--- src/Widget.js
+++ src/Widget.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import classSet from 'react-classset';
 
 import './Widget.css';
 
 class Widget extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {maximized: false};
+  }
+
   render() {
     return (
-      <div className="Widget">
+      <div className={classSet({Widget: true, maximized: this.state.maximized})}>
         <h4>Broomstick</h4>
+        <button onClick={() => this.toggleMaximized()}>Maximize/minimize</button>
       </div>
     );
   }
+  
+  toggleMaximized() {
+    this.setState(({maximized: !this.state.maximized}));
+  }
+
 }
 
 export default Widget;
