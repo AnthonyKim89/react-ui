68af331ca368ab522a6bbb8574accfc4a14bab04
Anthony89 <anthony.kim.89@hotmail.com>
2016-11-07 09:04:18 +0200

    configure responsive breakpoints for react-grid-layout.

--- src/widget/WidgetGrid.js
+++ src/widget/WidgetGrid.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactGridLayout, { WidthProvider } from 'react-grid-layout';
+import { Responsive, WidthProvider } from 'react-grid-layout';
 import moment from 'moment';
 import WidgetContainer from './WidgetContainer';
 
@@ -9,13 +9,16 @@ import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import './WidgetGrid.css';
 
-const GridLayout = WidthProvider(ReactGridLayout);
+const GridLayout = WidthProvider(Responsive);
 
 class WidgetGrid extends Component {
   render() {
     const time = moment('2016-08-03');
     return (
-      <GridLayout className="WidgetGrid" cols={8} rowHeight={30}>
+      <GridLayout className="WidgetGrid"
+                  breakpoints={{large: 1024, medium: 512, small: 0}}
+                  cols={{large: 8, medium: 4, small: 1}}
+                  rowHeight={30}>
         <div key={1} data-grid={{x: 0, y: 0, w: 3, h: 12}}>
           <WidgetContainer>
             <torqueAndDragBroomstick.Widget jobId={1029} time={time} />
