d5f0b4ed13bd5dfda35ab73f8128dc7e706063d7
Anthony89 <anthony.kim.89@hotmail.com>
2016-12-12 18:04:02 +0200

    Group widget types by category in Add Widget dialog.

--- src/widgets/components/AddWidgetDialog.js
+++ src/widgets/components/AddWidgetDialog.js
@@ -6,6 +6,11 @@ import './AddWidgetDialog.css';
 
 class AddWidgetDialog extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {expandedCategories: {}}
+  }
+
   render() {
     return <div className="c-add-widget-dialog">
       <Button className="c-add-widget-dialog__cancel-button"
@@ -16,17 +21,49 @@ class AddWidgetDialog extends Component {
         Add Widget to Dashboard
       </h3>
       <ul className="c-add-widget-dialog__widget-type-list">
-        {this.props.widgetTypes.valueSeq().map(widgetType =>
-          <li key={widgetType.constants.NAME}
-              className="c-add-widget-dialog__widget-type-list-item">
-            <button className="c-add-widget-dialog__add-widget-button"
-                    onClick={() => this.props.onWidgetAdd(widgetType)}>
-              {widgetType.constants.TITLE}
-            </button>
-          </li>
-        )}
+        {this.getWidgetTypesByCategory().map(([category, types]) => {
+          const categoryItem = this.renderCategoryListItem(category);
+          if (this.state.expandedCategories[category]) {
+            return [categoryItem].concat(types.map(t => this.renderWidgetTypeListItem(t)));
+          } else {
+            return categoryItem;
+          }
+        })}
       </ul>
     </div>;
+  }
+
+  renderCategoryListItem(category) {
+    return <li key={category}
+        className="c-add-widget-dialog__widget-type-list-item is-category">
+      <button className="c-add-widget-dialog__widget-type-list-button"
+              onClick={() => this.toggleCategory(category)}>
+        {category}
+      </button>
+    </li>
+  }
+
+  renderWidgetTypeListItem(widgetType) {
+    return <li key={widgetType.constants.NAME}
+        className="c-add-widget-dialog__widget-type-list-item is-widget-type">
+      <button className="c-add-widget-dialog__widget-type-list-button"
+              onClick={() => this.props.onWidgetAdd(widgetType)}>
+        {widgetType.constants.TITLE}
+      </button>
+    </li>;
+  }
+
+  getWidgetTypesByCategory() {
+    return this.props.widgetTypes
+      .valueSeq()
+      .groupBy(widgetType => widgetType.constants.CATEGORY_TITLE)
+      .entrySeq();
+  }
+
+  toggleCategory(category) {
+    this.setState(state => ({
+      expandedCategories: {...state.expandedCategories, [category]: !state.expandedCategories[category]}
+    }));
   }
 
 }
--- src/widgets/torqueAndDragBroomstick/constants.js
+++ src/widgets/torqueAndDragBroomstick/constants.js
@@ -1,5 +1,6 @@
 export const CATEGORY = 'torqueAndDrag';
 export const NAME = 'broomstick';
+export const CATEGORY_TITLE = 'Torque & Drag';
 export const TITLE = 'Trend Broomstick';
 export const SUBTITLE = 'Visual trend identification for hole problems or poor cleaning';
 export const INITIAL_SIZE = {w: 5, h: 15};
