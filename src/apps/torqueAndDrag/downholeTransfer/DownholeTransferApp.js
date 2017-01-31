ffb860365146dea810da7da966015189c9e0bd31
Anthony89 <anthony.kim.89@hotmail.com>
2017-01-31 13:10:16 +0200

    Downhole transfer app data table (not styled yet)

--- src/apps/torqueAndDrag/downholeTransfer/DownholeTransferApp.js
+++ src/apps/torqueAndDrag/downholeTransfer/DownholeTransferApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
+import { SUBSCRIPTIONS } from './constants';
 import LoadingIndicator from '../../../common/LoadingIndicator';
 
 import './DownholeTransferApp.css'
@@ -10,9 +11,44 @@ class DownholeTransferApp extends Component {
   render() {
     return (
       <div className="c-tnd-downhole-transfer">
-        <LoadingIndicator />
+        {this.getSubscriptionData() ?
+          this.renderTable() :
+          <LoadingIndicator />}
+        
       </div>
     );
+  }
+
+  renderTable() {
+    const data = this.getSubscriptionData().get('data');
+    return <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Surface</th>
+          <th></th>
+          <th>Downhole</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>TOR kft-lbf</th>
+          <td>{data.getIn(['surface', 'torque'])}</td>
+          <td>{data.get('torque_efficiency')}</td>
+          <td>{data.getIn(['downhole', 'torque'])}</td>
+        </tr>
+        <tr>
+          <th>WOB klbf</th>
+          <td>{data.getIn(['surface', 'weight_on_bit'])}</td>
+          <td>{data.get('weight_on_bit_efficiency')}</td>
+          <td>{data.getIn(['downhole', 'weight_on_bit'])}</td>
+        </tr>
+      </tbody>
+    </table>;
+  }
+
+  getSubscriptionData() {
+    return this.props.data && this.props.data.get(SUBSCRIPTIONS[0]);
   }
 
 }
