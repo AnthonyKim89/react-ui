8723114d373031cfb281749a421a9fed0cc5b089
Anthony89 <anthony.kim.89@hotmail.com>
2017-02-23 18:12:04 +0200

    Make corva-subscriptions WebSocket URL configurable at build time.

--- src/subscriptions/subscriptionClient.js
+++ src/subscriptions/subscriptionClient.js
@@ -3,10 +3,14 @@ import { fromJS } from 'immutable';
 
 import * as auth from '../auth';
 
+const socketUrl = process.env.REACT_APP_SUBSCRIPTIONS_URL ||
+                  process.env.REACT_APP_API_URL ||
+                  `${location.protocol}//${location.hostname}:3002`;
+
 let socket;
 
 export const connect = (onReceiveData) => {
-  socket = io(`${location.protocol}//${location.hostname}:3002`);
+  socket = io(socketUrl);
   socket.on('connect', () => {
     socket.emit('authenticate', {token: auth.getToken()})
   });
