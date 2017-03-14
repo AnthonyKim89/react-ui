5a0f9c97236c24e9a52b519bad31d7dd7913959a
Anthony89 <anthony.kim.89@hotmail.com>
2017-03-14 16:52:05 -0500

    Changing the heatmap apps so the data preparation is done in the app instead of the component.

--- src/apps/drillingEfficiency/ropHeatmap/ROPHeatmapApp.js
+++ src/apps/drillingEfficiency/ropHeatmap/ROPHeatmapApp.js
@@ -11,13 +11,58 @@ import './ROPHeatmapApp.css'
 class ROPHeatmapApp extends Component {
 
   render() {
-    return (
-      <div className="c-de-ropheatmap">
-        {subscriptions.selectors.firstSubData(this.props.data, SUBSCRIPTIONS) ?
-          <Heatmap title="ROP Heatmap" dataNode={"rotary"} data={subscriptions.selectors.firstSubData(this.props.data, SUBSCRIPTIONS)} /> :
-          <LoadingIndicator />}
-      </div>
-    );
+    try {
+      let rawData = subscriptions.selectors.firstSubData(this.props.data, SUBSCRIPTIONS).get("data");
+      return (
+        <div className="c-de-ropheatmap">
+          <Heatmap title="ROP Heatmap"
+                   series={this.getSeries(rawData)}
+                   xAxis={this.getAxis(rawData.get("x_axis"))}
+                   yAxis={this.getAxis(rawData.get("y_axis"))} />
+        </div>
+      );
+    } catch (e) {
+      return <div className="c-de-ropheatmap"><LoadingIndicator /></div>
+    }
+  }
+
+  getAxis(axis) {
+    let rowHeight = (axis.get("maximum") - axis.get("minimum"))/axis.get("rows");
+    return {
+      categories: this.getAxisData(axis.get("minimum"), rowHeight, axis.get("rows")),
+      title: axis.get("type").toUpperCase(),
+    }
+  }
+
+  getAxisData(start, unitSize, axisLength) {
+    let data = [];
+    for (let i = 0; i < axisLength; i++) {
+      let nextStart = start + unitSize;
+      data.push(Math.round(start));
+      start = nextStart;
+    }
+    return data;
+  }
+
+  getSeries(data) {
+    let series = [];
+    let rows = data.get("y_axis").get("rows");
+    let columns = data.get("x_axis").get("columns");
+    for (let y = 0; y < rows; y++) {
+      let row = data.get("rotary").get(y);
+      for (let x = 0; x < columns; x++) {
+        let z = row.get(x);
+        series.push([y, x, z]);
+      }
+    }
+    return {
+      name: "ROP",
+      data: series,
+      borderWidth: 1,
+      borderColor: "#444444",
+      nullColor: "#535353",
+      animation: false
+    };
   }
 }
 
