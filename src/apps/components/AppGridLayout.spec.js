27cb183fba0e1626ec5fa82dec5580ce2d983487
Anthony89 <anthony.kim.89@hotmail.com>
2017-03-16 14:54:22 -0400

    Misconfigured apps should not bring down the entire frontend.

---
+++ src/apps/components/AppGridLayout.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Map, Seq } from 'immutable';
+
+import AppGridLayout from './AppGridLayout';
+import { Size } from '../../common/constants';
+
+it('renders a fallback when mismatching app registry state exists', () => {
+  global.console.log = jest.fn();
+  const noop = () => {};
+  const fakeCoordinates = Map({
+    x: 1,
+    y: 2
+  });
+  const fakeApp = Map({
+    id: 1,
+    category: 'doesnotexist',
+    name: 'sorry',
+    coordinates: fakeCoordinates
+  });
+  const emptySeq = Seq([fakeApp]);
+  const emptyMap = Map();
+  const location = { query: { maximize: 'nope' } };
+  const wrapper = shallow(
+    <AppGridLayout
+      apps={emptySeq}
+      appData={emptyMap}
+      appAssets={emptyMap}
+      onAppSubscribe={noop}
+      onAppUnsubscribe={noop}
+      onAppMove={noop}
+      onAppSettingsUpdate={noop}
+      onAppAdd={noop}
+      onAppRemove={noop}
+      onAssetModified={noop}
+      isNative={true}
+      location={location}
+    />);
+  expect(console.log).toBeCalledWith('No UI app found for doesnotexist:sorry.');
+});
