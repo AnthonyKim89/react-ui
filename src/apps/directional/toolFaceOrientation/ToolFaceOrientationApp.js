255256a3acf3c836c08bbfbead440b426ecbb9c4
Anthony89 <anthony.kim.89@hotmail.com>
2017-03-27 13:37:51 -0600

    Changed accuracy styles. Added WIP TFO output

--- src/apps/directional/toolFaceOrientation/ToolFaceOrientationApp.js
+++ src/apps/directional/toolFaceOrientation/ToolFaceOrientationApp.js
@@ -1,22 +1,40 @@
 import React, { Component, PropTypes } from 'react';
+import { Icon } from 'react-materialize';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 // import { Row, Col } from 'react-materialize';
 
-// import { SUBSCRIPTIONS } from './constants';
-// import LoadingIndicator from '../../../common/LoadingIndicator';
-// import subscriptions from '../../../subscriptions';
+import { SUBSCRIPTIONS } from './constants';
+import LoadingIndicator from '../../../common/LoadingIndicator';
+import subscriptions from '../../../subscriptions';
 
 import './ToolFaceOrientationApp.css'
+
 
 class ToolFaceOrientationApp extends Component {
   render() {
     return (
+      <div className="c-di-tfo">
+        {this.getSubscriptionData() ?
+          this.renderData() :
+          <LoadingIndicator />}
+        
+      </div>
+    );
+  } 
+
+  renderData() {
+    const data = this.getSubscriptionData().get('data');    
+    return (
       <div className="c-di-toolface">
         <div className="gaps"></div>
-        <h1>Coming soon</h1>
+        {data.getIn(['data', 'slides']).last().get("tfo")} <span>°</span>
       </div>
     )
-  } 
+  }
+
+  getSubscriptionData() {
+    return subscriptions.selectors.firstSubData(this.props.data, SUBSCRIPTIONS);
+  }
 }
 
 ToolFaceOrientationApp.propTypes = {
