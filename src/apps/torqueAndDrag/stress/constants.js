eb30e2ec4951dc7dbb3ce675008dea8b3622afbb
Anthony89 <anthony.kim.89@hotmail.com>
2017-03-29 19:45:34 -0600

    Added missing T&D subtitles and fixed subtitle wrapping

--- src/apps/torqueAndDrag/stress/constants.js
+++ src/apps/torqueAndDrag/stress/constants.js
@@ -6,7 +6,7 @@ export const SUBSCRIPTIONS = [
 export const METADATA = {
   title: 'Stress',
   settingsTitle: 'Stress',
-  subtitle: '',
+  subtitle: 'Stresses applied to drillstring',
   developer: {name: 'Corva', url: 'http://www.corva.ai/'},
   version: 'v2.1',
   publishedAt: '2016-07-01T00:00:00'
