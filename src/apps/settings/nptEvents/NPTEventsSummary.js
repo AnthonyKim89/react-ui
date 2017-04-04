680bfdf59bbdf7ed42f60610b7964e86020c276d
Anthony89 <anthony.kim.89@hotmail.com>
2017-04-05 02:09:19 +0300

    fix indentation issue

--- src/apps/settings/nptEvents/NPTEventsSummary.js
+++ src/apps/settings/nptEvents/NPTEventsSummary.js
@@ -5,37 +5,36 @@ import moment from 'moment';
 
 import './NPTEventsSummary.css';
 
-class NPTEventsSummary extends Component { 
+class NPTEventsSummary extends Component {
   render() {
-    
     return (
-    	<div className="c-npt-summary">
-    		<Table centered={true} responsive={true}>
-    			<tbody>
-    				<tr>
-    					<td>
-    						Events
-    						<h4>{this.props.records.size}</h4>
-    					</td>
-    					<td>
-    						NPT Time 
-    						<h4>{this.getTotalNPTTime()}</h4>
-    					</td>
-    					<td>
-    						<Button floating large className='lightblue' waves='light' icon='add' onClick={this.props.onAdd} />
-    					</td>
-    				</tr>
-    			</tbody>
-    		</Table>
+      <div className="c-npt-summary">
+        <Table centered={true} responsive={true}>
+          <tbody>
+            <tr>
+              <td>
+                Events
+                <h4>{this.props.records.size}</h4>
+              </td>
+              <td>
+                NPT Time 
+                <h4>{this.getTotalNPTTime()}</h4>
+              </td>
+              <td>
+                <Button floating large className='lightblue' waves='light' icon='add' onClick={this.props.onAdd} />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
-    );
+    );    
   }
-
+  
   getTotalNPTTime() {
     let sum = 0;
     this.props.records.map((record) => {
-        sum = sum + record.getIn(["data","end_time"]) - record.getIn(["data","start_time"]);
-        return record;
+      sum = sum + record.getIn(["data","end_time"]) - record.getIn(["data","start_time"]);
+      return record;
     });
     
     let duration = moment.duration(sum*1000);
@@ -44,28 +43,25 @@ class NPTEventsSummary extends Component {
     let m= duration.minutes();
     let result="";
     if (d>0 || h>0) {
-        if (d>0) {
-            result+= d + " days ";
-        }        
-        if (h-d*24>0) {
-            result+= (h - d*24) + " hrs";
-        }
-        return result;
+      if (d>0) {
+        result+= d + " days ";
+      }        
+      if (h-d*24>0) {
+        result+= (h - d*24) + " hrs";
+      }
+      return result;
     }
             
     if (m>0) {
-        return m+ " mins";
-    }
-    
+      return m+ " mins";
+    }    
     return 0;
   }
 }
 
-NPTEventsSummary.propTypes = {
-  
+NPTEventsSummary.propTypes = {  
   records: ImmutablePropTypes.list.isRequired,
   onAdd: PropTypes.func.isRequired,
-  
 };
 
 export default NPTEventsSummary;
