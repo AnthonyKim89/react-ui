bb844e57e92c89103a260bbd2c77a3b5b32f2f1e
Anthony89 <anthony.kim.89@hotmail.com>
2017-04-10 14:56:30 -0500

    Added style fixes to header of directional slide and survey app. Used degree symbol, etc

--- src/apps/directional/slideSheet/SlideSheetApp.js
+++ src/apps/directional/slideSheet/SlideSheetApp.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import numeral from 'numeral';
 
 import { SUBSCRIPTIONS } from './constants';
 import LoadingIndicator from '../../../common/LoadingIndicator';
@@ -30,21 +31,21 @@ class SlideSheetApp extends Component {
         <table>
           <thead>
             <tr>
-              <th>Depth</th>
-              <th>Length</th>
-              <th>Inc</th>
-              <th>Azi</th>
-              <th>DLS</th>
+              <th>Depth <span>({this.props.convert.getUnitDisplay('length')})</span></th>
+              <th>Length <span>({this.props.convert.getUnitDisplay('length')})</span></th>              
+              <th>Inc <span>(°)</span></th>
+              <th>Azi <span>(°)</span></th>
+              <th>DLS <span>(°/{this.props.convert.convertValue(100, 'length', 'ft').fixFloat(1)}{this.props.convert.getUnitDisplay('length')})</span></th>
             </tr>
           </thead>
           <tbody>
             { data.map( (t,index)=> {
               return (
               <tr key={index}>
-                <td>{this.props.convert.convertValue(t.get("measured_depth"), 'length', 'ft').fixFloat(1)} <sub> {this.props.convert.getUnitDisplay('length')}</sub></td>
-                <td>{this.props.convert.convertValue(t.get("length"), 'length', 'ft').fixFloat(1)} <sub> {this.props.convert.getUnitDisplay('length')}</sub></td>
-                <td>{t.get("inclination").fixFloat(2)} <sub>*</sub></td>
-                <td>{t.get("azimuth").fixFloat(2)} <sub>*</sub></td>
+                <td>{numeral(this.props.convert.convertValue(t.get("measured_depth"), 'length', 'ft')).format('0,0.0')}</td>
+                <td>{this.props.convert.convertValue(t.get("length"), 'length', 'ft').fixFloat(1)}</td>
+                <td>{t.get("inclination").fixFloat(2)}</td>
+                <td>{t.get("azimuth").fixFloat(2)}</td>
                 <td>{t.get("dls").fixFloat(2)}</td>
               </tr>
               );
