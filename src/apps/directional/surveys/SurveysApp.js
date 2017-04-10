47fee41a2f3b7c1c4785d2ec5ef258352bebf2e7
Anthony89 <anthony.kim.89@hotmail.com>
2017-04-10 05:05:32 +0300

    improvement on directional apps  * trend app: put drilling window serie to bottom  * survey: show full list in  full screen mode  * slidesheet: shwo full list in full screen mode

--- src/apps/directional/surveys/SurveysApp.js
+++ src/apps/directional/surveys/SurveysApp.js
@@ -9,39 +9,48 @@ import './SurveysApp.css';
 
 class SurveysApp extends Component {
   render() {
+
   	let json = subscriptions.selectors.firstSubData(this.props.data,SUBSCRIPTIONS);
+    if (!json) {
+      return (        
+        <LoadingIndicator/>
+      );
+    }
+
+    let data;
+    if (this.props.maximize) {
+      data = json.getIn(["data","actual"]);
+    }
+    else {
+      data = json.getIn(["data","actual"]).slice(0,5);
+    }
+
     return (
       <div className="c-di-surveys">
-        <div className="gaps"></div>
-        { json ?
-        	
-          <table>
-            <thead>
-              <tr>
-                <th>Depth</th>
-                <th>Inc</th>
-                <th>Azi</th>
-                <th>DLS</th>
-              </tr>
-            </thead>
-            <tbody>
-              { 
-
-              	json.getIn(["data","actual"]).slice(0,5).map( (t,index)=> {
-	                return (
-	                <tr key={index}>
-	                  <td>{this.props.convert.convertValue(t.get("measured_depth"), 'length', 'ft').fixFloat(1)} <sub> {this.props.convert.getUnitDisplay('length')}</sub></td>
-	                  <td>{t.get("inclination").fixFloat(2)} <sub>*</sub></td>
-	                  <td>{t.get("azimuth").fixFloat(2)} <sub>*</sub></td>
-	                  <td>{t.get("dls").fixFloat(2)}</td>
-	                </tr>
-                );
-
-              })}
-            </tbody>
-          </table>:
-        <LoadingIndicator/> 
-        }
+        <div className="gaps"></div>        	
+        <table>
+          <thead>
+            <tr>
+              <th>Depth</th>
+              <th>Inc</th>
+              <th>Azi</th>
+              <th>DLS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+            	data.map( (t,index)=> {
+                return (
+                <tr key={index}>
+                  <td>{this.props.convert.convertValue(t.get("measured_depth"), 'length', 'ft').fixFloat(1)} <sub> {this.props.convert.getUnitDisplay('length')}</sub></td>
+                  <td>{t.get("inclination").fixFloat(2)} <sub>*</sub></td>
+                  <td>{t.get("azimuth").fixFloat(2)} <sub>*</sub></td>
+                  <td>{t.get("dls").fixFloat(2)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>        
       </div>
     );
   }
