b091d61166630ce72e91856a488a527cddc6ba64
Anthony89 <anthony.kim.89@hotmail.com>
2017-04-12 14:50:41 +0300

    save casing data as imperial

--- src/apps/settings/casing/CasingItem.js
+++ src/apps/settings/casing/CasingItem.js
@@ -2,18 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Input, Button} from 'react-materialize';
-import { Map } from 'immutable';
 
 import './CasingItem.css';
 
-const MIN_ID = 0.7;
-const MAX_ID = 100;
-const MIN_OD = 1;
-const MAX_OD = 100;
-const MIN_TOP_DEPTH = -200;
-const MAX_TOP_DEPTH = 30000;
-const MIN_BOTTOM_DEPTH = -200;
-const MAX_BOTTOM_DEPTH = 30000;
+const MIN_ID = 0.7; //in
+const MAX_ID = 100; //in
+const MIN_OD = 1; //in
+const MAX_OD = 100; //in
+const MIN_TOP_DEPTH = -200; //ft
+const MAX_TOP_DEPTH = 30000; //ft
+const MIN_BOTTOM_DEPTH = -200; //ft
+const MAX_BOTTOM_DEPTH = 30000; //ft
 
 
 class CasingItem extends Component { 
@@ -34,8 +33,18 @@ class CasingItem extends Component {
       errors:{}
     };
 
-    this.onCalcLinearMass = this.onCalcLinearMass.bind(this);    
-    this.onCalcLength = this.onCalcLength.bind(this);    
+    //convert range value
+    this.U_MIN_ID = props.convert.convertValue(MIN_ID,"shortLength","in");
+    this.U_MAX_ID = props.convert.convertValue(MAX_ID,"shortLength","in");
+    this.U_MIN_OD = props.convert.convertValue(MIN_OD,"shortLength","in");
+    this.U_MAX_OD = props.convert.convertValue(MAX_OD,"shortLength","in");
+    this.U_MIN_TOP_DEPTH = props.convert.convertValue(MIN_TOP_DEPTH,"length","ft");
+    this.U_MAX_TOP_DEPTH = props.convert.convertValue(MAX_TOP_DEPTH,"length","ft");
+    this.U_MIN_BOTTOM_DEPTH = props.convert.convertValue(MIN_BOTTOM_DEPTH,"length","ft");
+    this.U_MAX_BOTTOM_DEPTH = props.convert.convertValue(MAX_BOTTOM_DEPTH,"length","ft");
+
+    this.onCalcLinearMass = this.onCalcLinearMass.bind(this);
+    this.onCalcLength = this.onCalcLength.bind(this);
   }
   
   render() {
@@ -49,7 +58,7 @@ class CasingItem extends Component {
         <td className="hide-on-med-and-down">{this.props.convert.convertValue(parseFloat(top_depth), "length", "ft").fixFloat(2)}</td>
         <td className="hide-on-med-and-down">{this.props.convert.convertValue(parseFloat(bottom_depth), "length", "ft").fixFloat(2)}</td>
         <td>{this.props.convert.convertValue(parseFloat(length), "length", "ft").fixFloat(2)}</td>
-        <td className="hide-on-med-and-down">{parseFloat(linear_mass).fixFloat(2)}</td>
+        <td className="hide-on-med-and-down">{this.props.convert.convertValue(parseFloat(linear_mass), "force", "lbf").fixFloat(2)}</td>
         <td className="hide-on-med-and-down">{grade}</td>
         <td className="hide-on-med-and-down">
           <Button floating className='lightblue view-action' waves='light' icon='edit'
@@ -107,7 +116,7 @@ class CasingItem extends Component {
           <Input type="number"
             s={12}
             label="Linear Mass"
-            defaultValue={linear_mass? parseFloat(this.state.data.linear_mass).fixFloat(2): linear_mass}
+            defaultValue={linear_mass? this.props.convert.convertValue(parseFloat(linear_mass), "force", "lbf").fixFloat(2): linear_mass}
             ref="linearMassInput"
             error={this.state.errors.linear_mass}
             onChange={e => this.setState({data: Object.assign({},this.state.data,{linear_mass: e.target.value})})} />
@@ -166,7 +175,7 @@ class CasingItem extends Component {
   onCalcLinearMass() {
     let {id,od} = this.state.data;
 
-    if (this.isValidNumber(id,MIN_ID,MAX_ID) && this.isValidNumber(od,MIN_OD,MAX_OD) && this.isValidNumber(id,MIN_ID,od)) {
+    if (this.isValidNumber(id,this.U_MIN_ID,this.U_MAX_ID) && this.isValidNumber(od,this.U_MIN_OD,this.U_MAX_OD) && this.isValidNumber(id,this.U_MIN_ID,od)) {
       let calLW = this.deriveLinearMass(id,od);
       this.setState({data: Object.assign({},this.state.data,{linear_mass: calLW})});
       ReactDOM.findDOMNode(this.refs.linearMassInput).children[0].value = calLW.fixFloat(2) ;
@@ -178,49 +187,53 @@ class CasingItem extends Component {
   onCalcLength() {
     let {top_depth,bottom_depth} = this.state.data;
 
-    if (this.isValidNumber(top_depth,MIN_TOP_DEPTH,MAX_TOP_DEPTH) && this.isValidNumber(bottom_depth,MIN_BOTTOM_DEPTH,MAX_BOTTOM_DEPTH) && this.isValidNumber(top_depth,MIN_TOP_DEPTH,bottom_depth)) {
+    if (this.isValidNumber(top_depth,this.U_MIN_TOP_DEPTH,this.U_MAX_TOP_DEPTH) && this.isValidNumber(bottom_depth,this.U_MIN_TOP_DEPTH,this.U_MAX_TOP_DEPTH) && this.isValidNumber(top_depth,this.U_MIN_TOP_DEPTH,bottom_depth)) {
       this.setState({data: Object.assign({},this.state.data,{length: bottom_depth-top_depth})});
     }
 
   }
 
   hasFormErrors() {
+
     let {id,od,top_depth,bottom_depth,linear_mass} = this.state.data;
 
     let hasErrors = false;
     let errors = {};
 
-    if (!this.isValidNumber(id,MIN_ID,MAX_ID)) {
-      errors["id"] = `I.D must be ${MIN_ID}~${MAX_ID} in`;
+    let shortLengthUnitDisplay = this.props.convert.getUnitDisplay('shortLength');
+    let lengthUnitDisplay = this.props.convert.getUnitDisplay('length');
+
+    if (!this.isValidNumber(id,this.U_MIN_ID,this.U_MAX_ID)) {
+      errors["id"] = `I.D must be ${this.U_MIN_ID}~${this.U_MAX_ID} ${shortLengthUnitDisplay}`;
       hasErrors = true;
     }
 
-    if (!this.isValidNumber(od,MIN_OD,MAX_OD)) {
-      errors["od"] = `O.D must be ${MIN_OD}~${MAX_OD} in`;
+    if (!this.isValidNumber(od,this.U_MIN_OD,this.U_MAX_OD)) {
+      errors["od"] = `O.D must be ${this.U_MIN_OD}~${this.U_MAX_OD} ${shortLengthUnitDisplay}`;
       hasErrors = true;
     }
 
-    if (this.isValidNumber(id,MIN_ID,MAX_ID) && this.isValidNumber(od,MIN_OD,MAX_OD) && !this.isValidNumber(id,MIN_ID,od)) {
+    if (this.isValidNumber(id,this.U_MIN_ID,this.U_MAX_ID) && this.isValidNumber(od,this.U_MIN_OD,this.U_MAX_OD) && !this.isValidNumber(id,this.U_MIN_ID,od)) {
       errors["od"] = "O.D must be greater than I.D";
       hasErrors = true;      
     }
     
-    if (!this.isValidNumber(top_depth,MIN_TOP_DEPTH,MAX_TOP_DEPTH)) {
-      errors["top_depth"] = `Top depth must be in ${MIN_TOP_DEPTH}~${MAX_TOP_DEPTH} ft`;
+    if (!this.isValidNumber(top_depth,this.U_MIN_TOP_DEPTH,this.U_MAX_TOP_DEPTH)) {
+      errors["top_depth"] = `Top depth must be in ${this.U_MIN_TOP_DEPTH}~${this.U_MAX_TOP_DEPTH} ${lengthUnitDisplay}`;
       hasErrors = true;
     }
 
-    if (!this.isValidNumber(bottom_depth,MIN_BOTTOM_DEPTH,MAX_BOTTOM_DEPTH)) {
-      errors["bottom_depth"] = `Bottom Depth must be in ${MIN_BOTTOM_DEPTH} ~ ${MAX_BOTTOM_DEPTH} ft`;
+    if (!this.isValidNumber(bottom_depth,this.U_MIN_TOP_DEPTH,this.U_MAX_TOP_DEPTH)) {
+      errors["bottom_depth"] = `Bottom Depth must be in ${this.U_MIN_TOP_DEPTH} ~ ${this.U_MAX_TOP_DEPTH} ${lengthUnitDisplay}`;
       hasErrors = true;
     }
 
-    if (this.isValidNumber(top_depth,MIN_TOP_DEPTH,MAX_TOP_DEPTH) && this.isValidNumber(bottom_depth,MIN_BOTTOM_DEPTH,MAX_BOTTOM_DEPTH) && !this.isValidNumber(top_depth,MIN_TOP_DEPTH,bottom_depth)) {
+    if (this.isValidNumber(top_depth,this.U_MIN_TOP_DEPTH,this.U_MAX_TOP_DEPTH) && this.isValidNumber(bottom_depth,this.U_MIN_TOP_DEPTH,this.U_MAX_TOP_DEPTH) && !this.isValidNumber(top_depth,this.U_MIN_TOP_DEPTH,bottom_depth)) {
       errors["bottom_depth"] = "Bottom Depth must be greater than Top Depth";
       hasErrors = true;      
     }
 
-    if (!this.isValidNumber(linear_mass,0)) {      
+    if (!this.isValidNumber(linear_mass,0)) {
       errors["linear_mass"] = "Invalid Linear Mass";
       hasErrors = true;
     }
@@ -232,11 +245,9 @@ class CasingItem extends Component {
       }
     }
 
-    
     this.setState({errors: errors});
     return hasErrors;
     
-
   }
   
   save() {
@@ -245,8 +256,16 @@ class CasingItem extends Component {
       return;
     }
 
-    const record = this.props.record.update('data', (oldMap) => {
-      return Map(this.state.data);
+    let {id,od,top_depth,bottom_depth,length,linear_mass,grade} = this.state.data;
+
+    const record = this.props.record.update('data', (oldMap) => {      
+      return oldMap.set("id",this.props.convert.convertValue(id, "shortLength", this.props.convert.getUnitPreference("shortLength"), "in"))
+        .set("od",this.props.convert.convertValue(od, "shortLength", this.props.convert.getUnitPreference("shortLength"), "in"))
+        .set("top_depth",this.props.convert.convertValue(top_depth, "length", this.props.convert.getUnitPreference("length"), "ft"))
+        .set("bottom_depth",this.props.convert.convertValue(bottom_depth, "length", this.props.convert.getUnitPreference("length"), "ft"))
+        .set("length",this.props.convert.convertValue(length, "length", this.props.convert.getUnitPreference("length"), "ft"))
+        .set("linear_mass",this.props.convert.convertValue(linear_mass, "force", this.props.convert.getUnitPreference("force"), "lbf"))
+        .set("grade",grade);
     });
 
     this.props.onSave(record);
