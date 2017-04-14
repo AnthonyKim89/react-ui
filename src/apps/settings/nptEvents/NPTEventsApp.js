f564164af62f1561a16fd05e5a681fef8cfe56ed
Anthony89 <anthony.kim.89@hotmail.com>
2017-04-13 21:00:16 -0500

    Changed provider key in constants:

--- src/apps/settings/nptEvents/NPTEventsApp.js
+++ src/apps/settings/nptEvents/NPTEventsApp.js
@@ -37,7 +37,7 @@ class NPTEventsApp extends Component {
   }
 
   async loadRecords(asset) {
-    const records = await api.getAppStorage(METADATA.recordDevKey, METADATA.recordCollection, asset.get('id'), Map({limit: 0}));    
+    const records = await api.getAppStorage(METADATA.recordProvider, METADATA.recordCollection, asset.get('id'), Map({limit: 0}));    
     this.setState({
       records: records.sortBy(r=>r.get("timestamp"))
     });
@@ -116,8 +116,8 @@ class NPTEventsApp extends Component {
     let savedRecord;
     try {
       savedRecord = record.has('_id')? 
-        await api.putAppStorage(METADATA.recordDevKey, METADATA.recordCollection, record.get('_id') , record) :
-        await api.postAppStorage(METADATA.recordDevKey, METADATA.recordCollection, record);      
+        await api.putAppStorage(METADATA.recordProvider, METADATA.recordCollection, record.get('_id') , record) :
+        await api.postAppStorage(METADATA.recordProvider, METADATA.recordCollection, record);      
     }
     catch(error) {
       this._notificationSystem.addNotification({
@@ -155,7 +155,7 @@ class NPTEventsApp extends Component {
 
   async removeRecord(record) {    
     try {
-      await api.deleteAppStorage(METADATA.recordDevKey, METADATA.recordCollection, record.get('_id'));      
+      await api.deleteAppStorage(METADATA.recordProvider, METADATA.recordCollection, record.get('_id'));      
     }
     catch(error) {
       this._notificationSystem.addNotification({
