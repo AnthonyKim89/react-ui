9275f727c2834ef9418cc1b336ff374dedf554c6
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-02 11:37:19 +0300

    surface equipment app

---
+++ src/apps/settings/surfaceEquipment/index.js
@@ -0,0 +1,10 @@
+import { List } from 'immutable';
+
+import SurfaceEquipmentApp from './SurfaceEquipmentApp';
+import * as constants from './constants';
+
+export default {
+  AppComponent: SurfaceEquipmentApp,
+  settings: List(),
+  constants
+};
