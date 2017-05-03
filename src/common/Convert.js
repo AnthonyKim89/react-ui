7bdc7414954111ea90a22cda954c7adaef176f6d
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-02 21:24:01 -0500

    Fixing incorrect unit name.

--- src/common/Convert.js
+++ src/common/Convert.js
@@ -31,7 +31,7 @@ class Convert {
         temperature: 'F',
         speed: 'ft/h',
         area: 'ft2',
-        torque: 'kft-lbf',
+        torque: 'ft-klbf',
         force: 'klbf',
         oil: 'bbl',
         yp: 'dsf',
