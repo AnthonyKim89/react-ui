3d01442be9c7712efba18bad76cdc54d6d6321e7
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-18 11:38:48 -0400

    Change look of settings page.

--- src/apps/components/AppSettingsDialog.js
+++ src/apps/components/AppSettingsDialog.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Row, Col, Button, Icon } from 'react-materialize';
+import { Row, Col, Button } from 'react-materialize';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { isEqual } from 'lodash';
 
@@ -9,7 +9,7 @@ class AppSettingsDialog extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {settings: props.currentSettings};   
+    this.state = {settings: props.currentSettings};
   }
 
   render() {
@@ -23,30 +23,33 @@ class AppSettingsDialog extends Component {
         </h4>
         {this.props.appType.constants.METADATA.subtitle &&
           <h5 className="c-app-settings__subtitle">{this.props.appType.constants.METADATA.subtitle}</h5>}
+        <ul className="c-app-settings__tabs">
+          <li><Button className="c-app-settings__tab-btn">Settings</Button></li>
+          <li><Button className="c-app-settings__tab-btn btn-flat">App Details</Button></li>
+          <li><Button className="c-app-settings__tab-btn btn-flat">View App Page</Button></li>
+          <li>
+            <Button
+              className="c-app-settings__tab-btn btn-flat red-text"
+              onClick={this.props.onAppRemove}>Remove App</Button>
+          </li>
+        </ul>
       </header>
       <Row className="c-app-settings__grid">
-          <Col s={6}>
-            {this.props.settingsEditors.map(editor => {
-              const name = editor.get('name');
-              const title = editor.get('title');
-              const Editor = editor.get('Editor');
-              return <div className="c-app-settings__editor" key={name}>
-                <h5>{title}</h5>
-                <Editor
-                  inSettingsEditor={true}
-                  currentValue={this.state.settings.get(name)}
-                  onChange={v => this.setState({settings: this.state.settings.set(name, v)})}
-                  appType={this.props.appType} />
-              </div>;
-            })}
-          </Col>
-          <Col s={6}>
-            <div className="c-app-settings__editor">
-              <Button className="grey white-text" onClick={this.props.onAppRemove}>
-                <Icon>delete</Icon> <span>Remove Widget</span>
-              </Button>
-            </div>
-          </Col>
+        <Col s={12}>
+          {this.props.settingsEditors.map(editor => {
+            const name = editor.get('name');
+            const title = editor.get('title');
+            const Editor = editor.get('Editor');
+            return <div className="c-app-settings__editor" key={name}>
+              <h5>{title}</h5>
+              <Editor
+                inSettingsEditor={true}
+                currentValue={this.state.settings.get(name)}
+                onChange={v => this.setState({settings: this.state.settings.set(name, v)})}
+                appType={this.props.appType} />
+            </div>;
+          })}
+        </Col>
       </Row>
     </div>;
   }
