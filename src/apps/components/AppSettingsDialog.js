cd4bc08318ede5cbde2fd31681aa56087a6c0336
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-18 13:18:54 -0400

    Switch between settings and description tabs.

--- src/apps/components/AppSettingsDialog.js
+++ src/apps/components/AppSettingsDialog.js
@@ -5,11 +5,22 @@ import { isEqual } from 'lodash';
 
 import './AppSettingsDialog.css';
 
+const TABS = {
+  SETTINGS: 'settings',
+  APP_DETAILS: 'appDetails'
+}
+
 class AppSettingsDialog extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {settings: props.currentSettings};
+    this.state = {
+      activeTab: TABS.SETTINGS,
+      settings: props.currentSettings
+    };
+    this.tabRenderers = {};
+    this.tabRenderers[TABS.SETTINGS] = this.renderSettings.bind(this);
+    this.tabRenderers[TABS.APP_DETAILS] = this.renderAppDetails.bind(this);
   }
 
   render() {
@@ -24,8 +35,22 @@ class AppSettingsDialog extends Component {
         {this.props.appType.constants.METADATA.subtitle &&
           <h5 className="c-app-settings__subtitle">{this.props.appType.constants.METADATA.subtitle}</h5>}
         <ul className="c-app-settings__tabs">
-          <li><Button className="c-app-settings__tab-btn">Settings</Button></li>
-          <li><Button className="c-app-settings__tab-btn btn-flat">App Details</Button></li>
+          <li>
+            <Button
+              className={this.isActiveTab(TABS.SETTINGS) ?
+                "c-app-settings__tab-btn" :
+                "c-app-settings__tab-btn btn-flat"}
+              onClick={() => this.handleTabClick(TABS.SETTINGS)}
+            >Settings</Button>
+          </li>
+          <li>
+            <Button
+              className={this.isActiveTab(TABS.APP_DETAILS) ?
+                "c-app-settings__tab-btn" :
+                "c-app-settings__tab-btn btn-flat"}
+              onClick={() => this.handleTabClick(TABS.APP_DETAILS)}
+            >App Details</Button>
+          </li>
           <li><Button className="c-app-settings__tab-btn btn-flat">View App Page</Button></li>
           <li>
             <Button
@@ -36,19 +61,7 @@ class AppSettingsDialog extends Component {
       </header>
       <Row className="c-app-settings__grid">
         <Col s={12}>
-          {this.props.settingsEditors.map(editor => {
-            const name = editor.get('name');
-            const title = editor.get('title');
-            const Editor = editor.get('Editor');
-            return <div className="c-app-settings__editor" key={name}>
-              <h5>{title}</h5>
-              <Editor
-                inSettingsEditor={true}
-                currentValue={this.state.settings.get(name)}
-                onChange={v => this.setState({settings: this.state.settings.set(name, v)})}
-                appType={this.props.appType} />
-            </div>;
-          })}
+          {this.renderTab()}
         </Col>
       </Row>
     </div>;
@@ -58,12 +71,54 @@ class AppSettingsDialog extends Component {
     this.props.onDone(this.state.settings);
   }
 
+  handleTabClick(newTab) {
+    if (this.isActiveTab(newTab)) {
+      return;
+    }
+    this.setState({activeTab: newTab});
+  }
+
+  isActiveTab(tab) {
+    return this.state.activeTab === tab;
+  }
 
   shouldComponentUpdate(nextProps, nextState) {
     // Only render when the state changes. shouldComponentUpdate will be called 
     // frequently when the parent app receives new subscription data. But, we 
     // don't want to update the settings page.
     return !isEqual(nextState, this.state);
+  }
+
+  renderTab() {
+    return this.tabRenderers[this.state.activeTab]();
+  }
+
+  renderSettings() {
+    return this.props.settingsEditors.map(editor => {
+      const name = editor.get('name');
+      const title = editor.get('title');
+      const Editor = editor.get('Editor');
+      return (
+        <div className="c-app-settings__editor" key={name}>
+          <h5>{title}</h5>
+          <Editor
+            inSettingsEditor={true}
+            currentValue={this.state.settings.get(name)}
+            onChange={v => this.setState({settings: this.state.settings.set(name, v)})}
+            appType={this.props.appType} />
+        </div>
+      );
+    });
+  }
+
+  renderAppDetails() {
+    return (
+      <div className="c-app-settings__description">
+        {this.props.appType.constants.METADATA.description ?
+          this.props.appType.constants.METADATA.description :
+          "No description is available for this app."}
+      </div>
+    );
   }
 
 }
