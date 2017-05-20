c0b86c885ebae806338752c447a84798c4cb46b4
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-20 04:46:10 -0500

    Setting up style for settings bar and adding display icon.

--- src/apps/trace/traces/TracesSettingsBar.js
+++ src/apps/trace/traces/TracesSettingsBar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { isEqual } from 'lodash';
+import { Icon } from 'react-materialize';
 
 import './TracesSettingsBar.css';
 
 class TracesSettingsBar extends Component {
   render() {
     return <div className="c-traces__settings-bar">
+      <div className="c-traces__settings-bar__setting">
+        <div className="c-traces__settings-bar__setting__icon"><Icon>video_label</Icon></div>
+        <div className="c-traces__settings-bar__setting__label">Display</div>
+      </div>
     </div>;
   }
 
