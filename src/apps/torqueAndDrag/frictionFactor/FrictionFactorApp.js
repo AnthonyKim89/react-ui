6bba7b6544c39ffe27cabe323d656c6d35ce4bb6
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-24 01:26:21 +0800

    update ff app to save value from inputbox

--- src/apps/torqueAndDrag/frictionFactor/FrictionFactorApp.js
+++ src/apps/torqueAndDrag/frictionFactor/FrictionFactorApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-materialize';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import NotificationSystem from 'react-notification-system';
 import { Map } from 'immutable';
 import { METADATA,SUBSCRIPTIONS } from './constants';
 import LoadingIndicator from '../../../common/LoadingIndicator';
@@ -12,6 +13,10 @@ import './FrictionFactorApp.css';
 
 class FrictionFactorApp extends Component {
 
+  componentDidMount() {
+    this._notificationSystem = this.refs.notificationSystem;
+  }
+
   render() {
     return (
       <div className="c-tnd-friction-factor">
@@ -22,6 +27,7 @@ class FrictionFactorApp extends Component {
             {this.renderFactor('Open Hole Pickup', 'open_hole_pickup')}
           </div> :
           <LoadingIndicator />}
+        <NotificationSystem ref="notificationSystem" noAnimation={true} />
       </div>
     );
   }
@@ -35,7 +41,10 @@ class FrictionFactorApp extends Component {
       </Col>
       <Col s={3}>
         <input type="number" className="c-tnd-friction-factor__input"
-               defaultValue={current.formatNumeral("0.00")}/>
+               defaultValue={current.formatNumeral("0.00")}
+               ref={fieldName} 
+               onKeyPress={this.handleKeyPress.bind(this)}
+               onBlur={this.handleBlur.bind(this)}/>
       </Col>
       <Col s={4} className="c-tnd-friction-factor__predicted">
         *predicted {predicted.formatNumeral("0.00")}
@@ -48,51 +57,102 @@ class FrictionFactorApp extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-
-    if (nextProps.data && !nextProps.data.equals(this.props.data)) {      
-      this.saveRecord(subscriptions.selectors.firstSubData(nextProps.data, SUBSCRIPTIONS));
-    }
-    
     return !!(
         (nextProps.data && !nextProps.data.equals(this.props.data)) ||
         (nextProps.coordinates && !nextProps.coordinates.equals(this.props.coordinates))
     );
   }
 
-  async saveRecord(subscriptionRecord) {
-    const records = await api.getAppStorage(
-        METADATA.recordProvider, 
-        METADATA.recordCollection,this.props.asset.get('id'),  
-        Map({
-          limit: 1,
-          sort: '{timestamp: -1}'
-    }));    
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      const inputData = this.getInputData();      
+      if (!inputData) {
+        this._notificationSystem.addNotification({
+          message: 'All values should be between 0 and 1.',
+          level: 'error'
+        });
+        return;
+      }
+      this.saveRecord(inputData);
+    }
+  }
 
+  handleBlur() {
+    const inputData = this.getInputData();
+    if (!inputData) {
+      this._notificationSystem.addNotification({
+          message: 'Value should be 0~1',
+          level: 'error'
+        });
+      return;      
+    }
+    this.saveRecord(inputData);
+  }
+
+  getInputData() {
+    let casingValue = this.refs.casing.value;
+    let slackoffValue = this.refs.open_hole_slackoff.value;
+    let pickupValue = this.refs.open_hole_pickup.value;
+
+    if (this.checkValidity(casingValue,0,1) &&  this.checkValidity(slackoffValue,0,1) && this.checkValidity(pickupValue,0,1)) {
+      return Map({
+        casing: casingValue,
+        open_hole_slackoff: slackoffValue,
+        open_hole_pickup: pickupValue
+      });
+    }
+
+    return null;    
+  }
+
+  checkValidity(val,min,max) {
+    if ( !isNaN(parseFloat(val)) && val < max && val > min) {
+      return true;
+    }
+    return false;
+  }
+
+  async saveRecord(inputData) {        
+    const records = await api.getAppStorage(        
+      METADATA.recordProvider, 
+      METADATA.recordCollection,this.props.asset.get('id'),  
+      Map({
+        limit: 1,
+        sort: '{timestamp: -1}'
+    }));
+    
     let record = Map({
       asset_id: this.props.asset.get('id'),
-      data: subscriptionRecord.get('data'),
-      timestamp: subscriptionRecord.get('timestamp'),
+      data: inputData
     });
-    
+
     if (typeof(records.get(0)) !== "undefined") {
-      const recentApiRecord = records.get(0);          
-      var difference = subscriptionRecord.get('timestamp') - recentApiRecord.get('timestamp'); 
-      var minutesDifference = Math.floor(difference/1000/60);        
-      if (minutesDifference < 10 ) {                
+      const recentApiRecord = records.get(0);
+      let minutesDifference = (this.getData().get('timestamp') - recentApiRecord.get('timestamp')) / 1000 / 60;
+      if (minutesDifference < 10 ) { //could it be minus???
         record = record.set('_id', recentApiRecord.get('_id'));        
       }      
-    }  
-   
+    } 
+
     try {
-      record.has('_id')? 
+
+      const isUpdate = record.has('_id');
+      
+      isUpdate ?
         await api.putAppStorage(METADATA.recordProvider, METADATA.recordCollection, record.get('_id') , record) :
         await api.postAppStorage(METADATA.recordProvider, METADATA.recordCollection, record);
+
+      this._notificationSystem.addNotification({
+        message: isUpdate? 'Updated successfully.': 'Created successfully.',
+        level: 'success'
+      });
+
     }
     catch(error) {
       console.log(error);     
     }
   }
-
+  
 }
 
 FrictionFactorApp.propTypes = {
