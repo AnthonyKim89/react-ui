af3d4d462f672bf2f7f91063fab3c59cdee8c92f
Anthony89 <anthony.kim.89@hotmail.com>
2017-05-25 14:07:10 -0500

    Fixing font and js issues with IE11.

--- src/extend_native.js
+++ src/extend_native.js
@@ -4,12 +4,58 @@ import numeral from 'numeral';
  * There is risk introduced by extending native components.
  * All extensions must have eslint exceptions annotated!
  */
+/*eslint no-extend-native: ["error", { "exceptions": ["Number", "Array"] }]*/
 
 // This is to avoid having to wrap conversions and other things with toFixed and parseFloat calls
-/*eslint no-extend-native: ["error", { "exceptions": ["Number"] }]*/
 Number.prototype.fixFloat = function(digits) {
   return parseFloat(this.toFixed(digits));
 };
 Number.prototype.formatNumeral = function(fmt) {
   return numeral(this).format(fmt);
 };
+
+// IE11 doesn't have Array.fill()
+if (!Array.prototype.fill) {
+  Object.defineProperty(Array.prototype, 'fill', {
+    value: function(value) {
+
+      // Steps 1-2.
+      if (this === null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      let O = Object(this);
+
+      // Steps 3-5.
+      let len = O.length >>> 0;
+
+      // Steps 6-7.
+      let start = arguments[1];
+      let relativeStart = start >> 0;
+
+      // Step 8.
+      let k = relativeStart < 0 ?
+        Math.max(len + relativeStart, 0) :
+        Math.min(relativeStart, len);
+
+      // Steps 9-10.
+      let end = arguments[2];
+      let relativeEnd = end === undefined ?
+        len : end >> 0;
+
+      // Step 11.
+      let final = relativeEnd < 0 ?
+        Math.max(len + relativeEnd, 0) :
+        Math.min(relativeEnd, len);
+
+      // Step 12.
+      while (k < final) {
+        O[k] = value;
+        k++;
+      }
+
+      // Step 13.
+      return O;
+    }
+  });
+}
